dea464b73cea9a9461c22b7747dee0f4b141e8cd
Niklas Lönn <niklas@bubblecraft.se>
2024-05-01 19:19:23 +0200

    take parameters

--- install.sh
+++ install.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
+if [ -z "$SOURCES" ]; then
+    echo "Error: specify SOURCES variable"
+fi
+
+if [ -z "$TARGET" ]; then
+    echo "Error: specify TARGET variable"
+fi
+
+
+sudo echo sources="$SOURCES" > /etc/movelink.service.conf
+sudo echo target="$TARGET" >> /etc/movelink.service.conf
+
 sudo wget -O /usr/local/sbin/movelink https://raw.githubusercontent.com/bubblecraft/movelink.service/main/src/bin/movelink
 sudo chmod +x /usr/local/sbin/movelink
